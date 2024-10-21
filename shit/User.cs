216d36aa2b0c8908c8e75ea594010fd159e9183b
basel.abdella <basil.0522002@gmail.com>
2024-10-21 11:30:22 +0300

    added user validation

--- shit/User.cs
+++ shit/User.cs
@@ -5,7 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CreditCard { get; set; }
         public string? Password { get; set; }
+        public string? PasswordConfirmation { get; set; }
 
     }
 }
